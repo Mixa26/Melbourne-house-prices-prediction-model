48d3637576d6466a9c46c954d636895985af35bb
Mihajlo <djmixa26.hockey@gmail.com>
2022-09-03 12:41:43 +0200

    Imputer added for Null values in columns 'Car' and 'YearBuilt', not a lot of improvement.

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.impute import SimpleImputer
 
 houses_full = pd.read_csv('houses\melb_data.csv')
 
 #houses_full.columns
 
-features = ['Rooms','Bathroom','Distance','Landsize','Longtitude','Lattitude']
+features = ['Rooms','Bathroom','Distance','Landsize','Longtitude','Lattitude','Car','YearBuilt']
 #columns with null values arent added yet as well as categorical values
 #we will deal with those later
 X = houses_full[features]
 y = houses_full.Price
 
-from sklearn.model_selection import train_test_split
+train_X, test_X, train_y, test_y = train_test_split(X,y,train_size=0.9,test_size=0.1)
 
-train_X, test_X, train_y, test_y = train_test_split(X,y)
+#we will go with imputing values into Null with knowledge of what we imputed approach
+
+cols_with_Nan = [column for column in train_X.columns if train_X[column].isnull().any()]
+
+for col in cols_with_Nan:
+    train_X[col+'_was_missing'] = train_X[col].isnull()
+    test_X[col+'_was_missing'] = test_X[col].isnull()
+
+imputer = SimpleImputer()
+imputer.fit(train_X)
+train_X = pd.DataFrame(imputer.transform(train_X))
+test_X = pd.DataFrame(imputer.transform(test_X))
 
 #mae hopefully wont be worse than this
 lowest_mae_for_trees = 1000000
