a9cf87d18a15b6d96c07eb8be94d8604540be87a
Mihajlo <djmixa26.hockey@gmail.com>
2022-09-04 13:47:21 +0200

    Cross validation added to value the model, and some of the features changed

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
 
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import cross_val_score
 
 houses_full = pd.read_csv('houses\melb_data.csv')
 
 #houses_full.columns
 
-features = ['Rooms','Bathroom','Distance','Landsize','Longtitude','Lattitude','Car','YearBuilt','Type','Method','CouncilArea','Regionname']
+#features = ['Rooms','Bathroom','Distance','Landsize','Longtitude','Lattitude','Car','YearBuilt','Type','Method','CouncilArea','Regionname']
+features = ['Rooms','Bathroom','Distance','Landsize','Longtitude','Lattitude','Car','Type','Method','CouncilArea','Regionname']
 #columns with null values arent added yet as well as categorical values
 #we will deal with those later
 X = houses_full[features]
@@ -48,7 +50,7 @@ lowest_mae_for_trees = 1000000
 #default i
 lowest_i_for_trees = 300
 
-def get_mae(trees):
+"""def get_mae(trees):
     model = RandomForestRegressor(n_estimators = trees, random_state = 1)
     pipeline = Pipeline(steps=[
         ('preprocessor', preprocessing),
@@ -88,13 +90,15 @@ for i in [1500,2000,2500,3000,4500,5000]:
     if (current < lowest_mae_for_nodes):
         lowest_mae_for_nodes = current
         lowest_i_for_nodes = i
-
-model = RandomForestRegressor(n_estimators = lowest_i_for_trees,max_leaf_nodes = lowest_i_for_nodes, random_state = 1)
+"""
+model = RandomForestRegressor(n_estimators = lowest_i_for_trees,max_leaf_nodes = 1500, random_state = 1)
 pipeline = Pipeline(steps=[
         ('preprocessor', preprocessing),
         ('model', model)
     ])
 pipeline.fit(train_X,train_y)
-prediction = pipeline.predict(test_X)
+scores = cross_val_score(pipeline,test_X,test_y,cv=5)
+#prediction = pipeline.predict(test_X)
 print("Final model mean score:")
-print(mean_absolute_error(test_y,prediction))
+print(scores.mean())
+#print(mean_absolute_error(test_y,prediction))
